9d694281f2595be7f0a89e26ba4f02df6a0d6f71
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-10-18 09:42:52 +0100

    update

---
+++ day7/day7.js
@@ -0,0 +1,13 @@
+var styles = {
+    "background-color": "lightgray",
+    "width": "500px",
+    "height": "300px"
+};
+ 
+var obj = document.getElementById("container");
+Object.assign(obj.style, styles);
+
+i=document.getElementbyId(world);
+i.styles.backgroundColor = "rgba(255,255,255)";
+i.style.color="#FFFFFF";
+i.style.fontFamily = "Helvetica";
